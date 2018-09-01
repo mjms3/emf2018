52a1c1e6e3321f47a111cdeb0f630a46709f577c
Daniel Saul <daniel@dansaul.co.uk>
2018-09-01 14:02:56 +0100

    Start of client app for Tilda badge

---
+++ client/main.py
@@ -0,0 +1,7 @@
+""" Tilder Dating
+"""
+___name___         = "Tilder Dating"
+___license___      = "MIT"
+___dependencies___ = ["wifi", "http", "ugfx_helper", "sleep"]
+___categories___   = ["Other"]
+___bootstrapped___ = True
