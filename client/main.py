c3af4a02edc9f0e91de9d9fbf2c6dce45085aba9
Daniel Saul <daniel@dansaul.co.uk>
2018-09-01 18:37:08 +0100

    Create profile finished

--- client/main.py
+++ client/main.py
@@ -6,11 +6,11 @@ ___dependencies___ = ["wifi", "http", "ugfx_helper", "sleep", "dialogs", "sim800
 ___categories___   = ["Other"]
 ___bootstrapped___ = True
 
-import app, buttons, ugfx, ugfx_helper, http, dialogs, sim800, database, json
+import app, buttons, ugfx, ugfx_helper, http, dialogs, sim800, database, ujson
 from tilda import Buttons
 
 running = True
-api_url = "http://blabla.com"
+api_url = "http://emf2018.us-east-2.elasticbeanstalk.com"
 
 ugfx_helper.init()
 ugfx.clear(ugfx.html_color(0x000000))
@@ -53,46 +53,39 @@ def create_profile():
     contact = dialogs.prompt_text("How should your matches contact you? :P")
     imei = sim800.imei()
 
-    ugfx.clear()
-    ugfx.text(5, 5, "A", ugfx.BLACK)
-
     profile = {
         'unique_identifier': imei,
         'username': name,
         'age': age,
         'tag_line': tag_line,
-        'temperature': "",
-        'humidity': "",
-        'magnetic_flux': "",
         'looking_for': looking_for,
         'contact': contact
     }
-    ugfx.text(5, 15, "B", ugfx.BLACK)
 
-    profile_json = json.dumps(profile)
+    profile_json = ujson.dumps(profile)
 
-    ugfx.text(5, 25, "C", ugfx.BLACK)
     try:
-        http.post(api_url+'/create_user', data=profile_json).raise_for_status().close()
+        http.post(api_url+'/create_user', json=profile).raise_for_status().close()
     except:
         ugfx.clear()
-        ugfx.text(5, 5, "Error creating profile. Try again later.", ugfx.BLACK)
+        ugfx.text(5, 100, "Error creating profile. Try again later. :(", ugfx.BLACK)
+        return False
 
-    ugfx.text(5, 35, "D", ugfx.BLACK)
-    quit_loop()
     database.set("tildr_profile", profile_json)
 
-
+    return True
 
 def quit_loop():
     while True:
         if buttons.is_triggered(Buttons.BTN_Menu):
             return
 
+
 while running:
     render_splash_screen()
     if not run_splash_screen():
         break
-    create_profile()
+    if not create_profile():
+        continue
 
 app.restart_to_default()
