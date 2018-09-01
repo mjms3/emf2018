a8d8a13d5b48fd7022830bb1468d5851dc912a4f
mjms3 <shepherd.max@gmail.com>
2018-09-01 12:46:09 +0000

    Create a basic user table for tildar

--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -8,18 +8,8 @@ class LoginUser(Base):
 
     login_user_id = Column(Integer,primary_key=True)
     username = Column(String,unique=True, nullable=False)
-
-class RequestToken(Base):
-    __tablename__ = 'request_token'
-
-    request_token_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('login_user.login_user_id'), nullable=False)
-    request_token = Column(String, unique=True, nullable=False)
-    requested_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-
-class SubmittedResult(Base):
-    __tablename__ = 'submitted_result'
-
-    submitted_result_id = Column(Integer, primary_key=True)
-    request_token_id = Column(Integer, ForeignKey('request_token.request_token_id'), nullable=False)
-    result_value = Column(Integer, nullable=False)
+    age = Column(Integer, nullable=False)
+    tag_line = Column(String, nullable=False)
+    temperature = Column(Numeric, nullable=False)
+    humidity = Column(Numeric, nullable=False)
+    magnetic_flux = Column(Numeric, nullable =False)
